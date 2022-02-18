962c920845895320b9775abb3aeede7996f3a97b
Michael Jang <michaeljang86@gmail.com>
2022-02-18 02:43:54 -0500

    Housekeeping - Removed unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import "./App.css";
 import Grid from "./Grid.js";
-import GridWithHooks from "./GridWithHooks.js";
 
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 function App() {
-  // const [count, setCount] = useState(0);
-  // useEffect(() => {
-  //   // Update the document title using the browser API
-  //   // setCount(count + 1);
-  //   document.title = `${count} clicks`;
-  // });
-  // return (
-  //   <div>
-  //     <p>You clicked {count} times</p>
-  //     <button onClick={() => setCount(count + 1)}>Click me</button>
-  //   </div>
-  // );
   return (
     <div className="App">
       {/* <h3>iLoveHue Clone</h3> */}
-      {/* <Grid width={10} height={3} /> */}
-      <GridWithHooks width={6} height={8} />
+      <Grid width={6} height={8} />
     </div>
   );
 }
--- src/Grid.js
+++ src/Grid.js
@@ -1,69 +1,35 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Block from "./Block.js";
+import { rgb, calculateHue } from "./utils.js";
 
-function rgb() {
-  return Math.floor(Math.random() * 256);
-}
+export default function Grid(dimensions) {
+  const [topLeft] = useState([rgb(), rgb(), rgb()]);
+  const [topRight] = useState([rgb(), rgb(), rgb()]);
+  const [bottomLeft] = useState([rgb(), rgb(), rgb()]);
+  const [bottomRight] = useState([rgb(), rgb(), rgb()]);
 
-export default class Grid extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      blocks: [],
-      won: false,
-      topLeft: [rgb(), rgb(), rgb()], // [255, 0, 0],
-      topRight: [rgb(), rgb(), rgb()], //  [0, 255, 255],
-      bottomLeft: [rgb(), rgb(), rgb()], //  [0, 0, 255],
-      bottomRight: [rgb(), rgb(), rgb()], //  [255, 128, 0],
-    };
-  }
+  let corners = { topLeft, topRight, bottomLeft, bottomRight };
 
-  calculateHue = (destination) => {
-    let { topLeft, topRight, bottomLeft, bottomRight } = this.state;
-    let { width, height } = this.props;
-    let [x, y] = destination;
-
-    let calcComponent = (i) => {
-      let start =
-        topLeft[i] + y * ((bottomLeft[i] - topLeft[i]) / (height - 1));
-      let end =
-        topRight[i] + y * ((bottomRight[i] - topRight[i]) / (height - 1));
-
-      let component = start + (x * (end - start)) / (width - 1);
-      return Math.round(component);
-    };
-
-    return [0, 1, 2].map((i) => calcComponent(i));
-  };
-
-  componentDidMount() {
+  const [blocks] = useState(() => {
     let blocks = [];
 
-    for (let y = 0; y < this.props.height; y++) {
-      for (let x = 0; x < this.props.width; x++) {
-        let block = {
+    [...Array(dimensions.height).keys()].forEach((y) => {
+      [...Array(dimensions.width).keys()].forEach((x) => {
+        blocks.push({
           location: [x, y],
-          // destination: [x, y],
-          colour: this.calculateHue([x, y]),
-        };
-        blocks.push(block);
-      }
-    }
-    console.log({ blocks });
-    this.setState({ blocks });
-  }
+          colour: calculateHue([x, y], corners, dimensions),
+        });
+      });
+    });
 
-  render() {
-    let { width, height } = this.props;
-    console.log({ width, height });
-    let style = {};
+    return blocks;
+  });
 
-    return (
-      <div style={style}>
-        {this.state.blocks.map((block, i) => (
-          <Block key={i} block={block} />
-        ))}
-      </div>
-    );
-  }
+  return (
+    <div>
+      {blocks.map((block, i) => (
+        <Block key={i} block={block} />
+      ))}
+    </div>
+  );
 }
